341701f76651746111755fe2abc8ecabcedde75a
jancarlohendriks <jancarlohendriks@hotmail.com>
2023-10-05 02:48:44 +0200

    stadium working

--- src/components/VirtualWardrobe/index.js
+++ src/components/VirtualWardrobe/index.js
@@ -3,155 +3,404 @@ import React, { useRef, useEffect } from "react";
 import { Vologram } from "volograms-js";
 import { Sky } from "three/addons/objects/Sky.js";
 import * as THREE from "three";
-// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-// import { GLTFLoader } from "three-gltf-loader";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-// import stadium from "@/assets/stadium.glb?url";
 import "./styles.scss";
 
-const VirtualWardobe = () => {
+const VirtualWardrobe = () => {
   const containerRef = useRef();
 
   useEffect(() => {
-    // Create a scene
-    const scene = new THREE.Scene();
-
-    // Create a camera
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
-    camera.position.z = 5;
-
-    // Create a renderer
+    // Create a scene, camera, and renderer
     const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    containerRef.current.appendChild(renderer.domElement);
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enablePan = false;
+    controls.minDistance = 1;
+    controls.maxDistance = 4;
 
-    scene.add(new THREE.AmbientLight(0xffffff, 1));
+    // Initialize the scene, sky, and model
+    initScene();
+    initSky();
+    loadModel();
+    initVologram();
 
-    // Create a GLTF loader
-    const loader = new GLTFLoader();
+    window.addEventListener("resize", onWindowResize, false);
 
-    // Load the GLB model
-    loader.load("assets/stadium.glb", (gltf) => {
-      const model = gltf.scene;
-      scene.add(model);
-      console.log(model);
+    animate();
 
-      // Optional: You can perform additional operations on the loaded model here.
+    // Function to initialize the Three.js scene
+    function initScene() {
+      const WIDTH = window.innerWidth;
+      const HEIGHT = WIDTH / (16 / 9);
 
-      // Create an instance of OrbitControls
-      const controls = new OrbitControls(camera, renderer.domElement);
-      controls.enableDamping = true; // Add damping for smoother camera movement
-      controls.dampingFactor = 0.05; // Adjust damping factor as needed
-      controls.rotateSpeed = 0.5; // Adjust rotation speed as needed
+      camera.aspect = WIDTH / HEIGHT;
+      camera.position.set(0, 1, 2);
+      camera.updateProjectionMatrix();
 
-      // Create an animation loop
-      const animate = () => {
-        requestAnimationFrame(animate);
+      renderer.toneMapping = THREE.ACESFilmicToneMapping;
+      renderer.toneMappingExposure = 0.7;
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      containerRef.current.appendChild(renderer.domElement);
+      scene.add(new THREE.AmbientLight(0xffffff, 1));
+      controls.target.set(0, 0.9, 0);
+      controls.update();
+    }
 
-        // Perform any animations or updates here.
+    // Function to initialize the sky
+    function initSky() {
+      const sky = new Sky();
+      sky.scale.setScalar(450000);
+      scene.add(sky);
 
-        renderer.render(scene, camera);
+      const effectController = {
+        turbidity: 10,
+        rayleigh: 3,
+        mieCoefficient: 0.005,
+        mieDirectionalG: 0.7,
+        elevation: 2,
+        azimuth: 180,
+        exposure: renderer.toneMappingExposure,
       };
 
-      animate();
-    });
+      const uniforms = sky.material.uniforms;
+      uniforms["turbidity"].value = effectController.turbidity;
+      uniforms["rayleigh"].value = effectController.rayleigh;
+      uniforms["mieCoefficient"].value = effectController.mieCoefficient;
+      uniforms["mieDirectionalG"].value = effectController.mieDirectionalG;
+
+      const sun = new THREE.Vector3();
+      const phi = THREE.MathUtils.degToRad(90 - effectController.elevation);
+      const theta = THREE.MathUtils.degToRad(effectController.azimuth);
+      sun.setFromSphericalCoords(1, phi, theta);
+      uniforms["sunPosition"].value.copy(sun);
+      renderer.toneMappingExposure = effectController.exposure;
+    }
+
+    // Function to load the GLTF model
+    function loadModel() {
+      // Load the GLB model
+      window.createImageBitmap = undefined;
+      const loader = new GLTFLoader();
+      loader.load("assets/stadium.glb", (gltf) => {
+        const model = gltf.scene;
+        model.scale.set(1, 1, 1);
+        model.position.set(7, 10.05, -8);
+        model.rotateY(-91);
+        scene.add(model);
+      });
+    }
+
+    // Function to handle window resize
+    function onWindowResize() {
+      const WIDTH = window.innerWidth;
+      const HEIGHT = WIDTH / (16 / 9);
+
+      camera.aspect = WIDTH / HEIGHT;
+      camera.updateProjectionMatrix();
+      renderer.setSize(WIDTH, HEIGHT);
+    }
+
+    // Function for the animation loop
+    function animate() {
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+    }
+
+    // Function to initialize the Vologram
+    function initVologram() {
+      const updateLoading = (p, play) => {
+        // const el = document.getElementById("loading");
+        // el.innerText = Math.round(p * 100) + "%";
+        vologram.elVideo.play();
+      };
+
+      let vologram = new Vologram(
+        "assets/left_1690983019490_ld",
+        updateLoading
+      );
+      scene.add(vologram);
+
+      // Play/Pause button and Sound/Mute button
+      document.getElementById("playpause").onclick = (e) =>
+        vologram.elVideo.paused
+          ? vologram.elVideo.play()
+          : vologram.elVideo.pause();
+
+      document.getElementById("left").onclick = (e) => {
+        vologram.clear();
+        vologram = new Vologram("assets/left_1690983019490_ld", updateLoading);
+        scene.add(vologram);
+      };
+
+      document.getElementById("head").onclick = (e) => {
+        vologram.clear();
+        vologram = new Vologram("assets/head_1690983148857_ld", updateLoading);
+        scene.add(vologram);
+      };
+
+      document.getElementById("right").onclick = (e) => {
+        vologram.clear();
+        vologram = new Vologram("assets/right_1690983080648_ld", updateLoading);
+        scene.add(vologram);
+      };
+    }
   }, []);
 
-  return <div ref={containerRef} />;
+  return (
+    <div id="interactive">
+      <div ref={containerRef}>
+        <div id="icon-click"></div>
+        <div id="gui">
+          <div id="gui-controls">
+            <div className="gui-controls__left">
+              <button className="gui-button" id="playpause">
+                <span></span>
+              </button>
+            </div>
+            <div className="gui-controls__right">
+              <span>Actions:</span>
+              &nbsp;
+              <button className="gui-button" id="left">
+                <span>Left</span>
+              </button>
+              <button className="gui-button" id="head">
+                <span>Head</span>
+              </button>
+              <button className="gui-button" id="right">
+                <span>Right</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default VirtualWardobe;
+export default VirtualWardrobe;
 
-// "use client";
-// import React, { useRef } from "react";
-// import { Canvas, extend, useLoader } from "@react-three/fiber";
-// import { Html, Sky, OrbitControls } from "@react-three/drei";
-// import { GLTFLoader } from "three/addons/loaders/GLTFLoader";
+// import React, { useRef, useEffect } from "react";
 // import { Vologram } from "volograms-js";
-// import { Suspense } from "react";
+// import { Sky } from "three/addons/objects/Sky.js";
+// import * as THREE from "three";
+// // import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+// // import { GLTFLoader } from "three-gltf-loader";
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+// // import stadium from "@/assets/stadium.glb?url";
 // import "./styles.scss";
-// // extend({ OrbitControls });
 
-// function Scene() {
-//   // const controls = useRef();
-//   // const { nodes } = useLoader(GLTFLoader, "./assets/stadium.glb");
-//   const Stadium = useLoader(GLTFLoader, "./assets/stadium.glb");
-//   // console.log(Stadium);
+// const VirtualWardobe = () => {
+//   // const containerRef = useRef();
+//   // const container = containerRef.current;
+//   // console.log(container);
 
-//   const updateLoading = (p, play) => {
-//     // Update loading UI here if needed
-//   };
+//   useEffect(() => {
+//     const container = document.getElementById("tryout");
+//     console.log(container);
+//     if (container) {
+//       const renderer = new THREE.WebGLRenderer({ antialias: true });
+//       const scene = new THREE.Scene();
+//       const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+//       const controls = new OrbitControls(camera, renderer.domElement);
+//       controls.enablePan = false;
+//       controls.minDistance = 1;
+//       controls.maxDistance = 4;
 
-//   // let vologram = new Vologram("assets/left_1690983019490_ld", updateLoading);
+//       // Initialize the scene, sky, and model
+//       initScene();
+//       initSky();
+//       loadModel();
+//       initVologram();
+
+//       // Event listeners
+//       document.addEventListener("DOMContentLoaded", () => {
+//         animate();
+//       });
+
+//       window.addEventListener("resize", onWindowResize, false);
+
+//       // Function to initialize the Three.js scene
+//       function initScene() {
+//         const WIDTH = container.clientWidth;
+//         const HEIGHT = WIDTH / (16 / 9);
+
+//         camera.aspect = WIDTH / HEIGHT;
+//         camera.position.set(0, 1, 2);
+//         camera.updateProjectionMatrix();
+
+//         renderer.toneMapping = THREE.ACESFilmicToneMapping;
+//         renderer.toneMappingExposure = 0.7;
+//         renderer.setPixelRatio(window.devicePixelRatio);
+//         renderer.setSize(WIDTH, HEIGHT);
+//         container && container.appendChild(renderer.domElement);
+//         scene.add(new THREE.AmbientLight(0xffffff, 1));
+//         controls.target.set(0, 0.9, 0);
+//         controls.update();
+//       }
+
+//       // Function to initialize the sky
+//       function initSky() {
+//         const sky = new Sky();
+//         sky.scale.setScalar(450000);
+//         scene.add(sky);
+
+//         const effectController = {
+//           turbidity: 10,
+//           rayleigh: 3,
+//           mieCoefficient: 0.005,
+//           mieDirectionalG: 0.7,
+//           elevation: 2,
+//           azimuth: 180,
+//           exposure: renderer.toneMappingExposure,
+//         };
+
+//         const uniforms = sky.material.uniforms;
+//         uniforms["turbidity"].value = effectController.turbidity;
+//         uniforms["rayleigh"].value = effectController.rayleigh;
+//         uniforms["mieCoefficient"].value = effectController.mieCoefficient;
+//         uniforms["mieDirectionalG"].value = effectController.mieDirectionalG;
+
+//         const sun = new THREE.Vector3();
+//         const phi = THREE.MathUtils.degToRad(90 - effectController.elevation);
+//         const theta = THREE.MathUtils.degToRad(effectController.azimuth);
+//         sun.setFromSphericalCoords(1, phi, theta);
+//         uniforms["sunPosition"].value.copy(sun);
+//         renderer.toneMappingExposure = effectController.exposure;
+//       }
+
+//       // function loaded(xhr) {
+//       //   const el = document.getElementById("loading");
+//       //   el.innerText = Math.round((xhr.loaded / xhr.total) * 100) + "%";
+
+//       //   // setTimeout(() => {
+//       //   //   el.classNameList.add("loaded");
+//       //   // }, 2000);
+//       // }
+
+//       // Function to load the GLTF model
+//       function loadModel() {
+//         const loader = new GLTFLoader();
+//         loader.load(
+//           "/assets/stadium.glb",
+//           function (gltf) {
+//             const model = gltf.scene;
+//             model.scale.set(1, 1, 1);
+//             model.position.set(7, 10.05, -8);
+//             model.rotateY(-91);
+//             scene.add(model);
+//             console.log(model);
+//           },
+//           (xhr) => console.log(xhr),
+//           (error) => console.log(error)
+//         );
+//       }
+
+//       // Function to handle window resize
+//       function onWindowResize() {
+//         const WIDTH = container.clientWidth;
+//         const HEIGHT = WIDTH / (16 / 9);
+
+//         camera.aspect = WIDTH / HEIGHT;
+//         camera.updateProjectionMatrix();
+//         renderer.setSize(WIDTH, HEIGHT);
+//       }
+
+//       // Function for the animation loop
+//       function animate() {
+//         requestAnimationFrame(animate);
+//         renderer.render(scene, camera);
+//       }
+
+//       // Function to initialize the Vologram
+//       function initVologram() {
+//         const updateLoading = (p, play) => {
+//           // const el = document.getElementById("loading");
+//           // el.innerText = Math.round(p * 100) + "%";
+//           vologram.elVideo.play();
+//         };
+
+//         let vologram = new Vologram(
+//           "assets/left_1690983019490_ld",
+//           updateLoading
+//         );
+//         scene.add(vologram);
+
+//         // Play/Pause button and Sound/Mute button
+//         document.getElementById("playpause").onclick = (e) =>
+//           vologram.elVideo.paused
+//             ? vologram.elVideo.play()
+//             : vologram.elVideo.pause();
+
+//         document.getElementById("left").onclick = (e) => {
+//           vologram.clear();
+//           vologram = new Vologram(
+//             "assets/left_1690983019490_ld",
+//             updateLoading
+//           );
+//           scene.add(vologram);
+//         };
+
+//         document.getElementById("head").onclick = (e) => {
+//           vologram.clear();
+//           vologram = new Vologram(
+//             "assets/head_1690983148857_ld",
+//             updateLoading
+//           );
+//           scene.add(vologram);
+//         };
+
+//         document.getElementById("right").onclick = (e) => {
+//           vologram.clear();
+//           vologram = new Vologram(
+//             "assets/right_1690983080648_ld",
+//             updateLoading
+//           );
+//           scene.add(vologram);
+//         };
+//       }
+//     }
+
+//     // Clean up OrbitControls when the component unmounts
+//     return () => {
+//       controls.dispose();
+//     };
+//   }, []);
 
 //   return (
-//     <>
-//       {/* <ambientLight intensity={1} /> */}
-//       <Sky />
-//       <orbitControls />
-//       <group position={[7, 10.05, -8]} rotation-y={-Math.PI / 2}>
-//         <primitive
-//           dispose={null}
-//           // key={nodes.uuid}
-//           object={Stadium}
-//           scale={[1, 1, 1]}
-//         />
-//       </group>
-//       {/* <VologramComponent vologram={vologram} /> */}
-//     </>
-//   );
-// }
-
-// function VologramComponent({ vologram }) {
-//   const playPause = () => {
-//     vologram.elVideo.paused
-//       ? vologram.elVideo.play()
-//       : vologram.elVideo.pause();
-//   };
-
-//   const changeVologram = (vologramName) => {
-//     vologram.clear();
-//     vologram = new Vologram(`assets/${vologramName}_ld`, updateLoading);
-//   };
-
-//   return (
-//     // <Html>
-//     <div>
-//       <button id="playpause" onClick={playPause}>
-//         Play/Pause
-//       </button>
-//       <button id="left" onClick={() => changeVologram("left_1690983019490")}>
-//         Left
-//       </button>
-//       <button id="head" onClick={() => changeVologram("head_1690983148857")}>
-//         Head
-//       </button>
-//       <button id="right" onClick={() => changeVologram("right_1690983080648")}>
-//         Right
-//       </button>
+//     <div id="interactive">
+//       <div id="tryout">
+//         <div id="icon-click"></div>
+//         <div id="gui">
+//           <div id="gui-controls">
+//             <div className="gui-controls__left">
+//               <button className="gui-button" id="playpause">
+//                 <span></span>
+//               </button>
+//             </div>
+//             <div className="gui-controls__right">
+//               <span>Actions:</span>
+//               &nbsp;
+//               <button className="gui-button" id="left">
+//                 <span>Left</span>
+//               </button>
+//               <button className="gui-button" id="head">
+//                 <span>Head</span>
+//               </button>
+//               <button className="gui-button" id="right">
+//                 <span>Right</span>
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//         <div id="loading">...</div>
+//       </div>
+//       {/* <div ref={containerRef} /> */}
 //     </div>
-//     // </Html>
 //   );
-// }
-
-// function VirtualWardobe() {
-//   return (
-//     <div id="tryout">
-//       <Canvas>
-//         <Html>
-//           <Suspense fallback={null}>
-//             <Scene />
-//           </Suspense>
-//         </Html>
-//       </Canvas>
-//     </div>
-//   );
-// }
+// };
 
 // export default VirtualWardobe;
